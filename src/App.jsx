228094f1ee9bbde0a3fc383c0162920a6f8b166c
Abdullah Al Faiyaz <abdullah-al.faiyaz@stud.uni-bamberg.de>
2025-11-04 23:54:01 +0100

    Added CoinCard components

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,76 @@
 import { useEffect } from "react";
 import { useState } from "react";
-const API_URL =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+import CoinCard from "./components/CoinCard";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  // using fetch .then
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => {
-        if (!res.ok) throw new Error("Failed to fetch data");
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setCoins(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error.message);
-        setLoading(false);
-      });
-  }, []);
 
   // useEffect(() => {
-  //   const fetchCoins = async () => {
-  //     try {
-  //       const res = await fetch(API_URL);
-  //       if (!res.ok) throw new Error("Failed to fetch data");
-  //       const data = await res.join();
+  //   fetch(API_URL)
+  //     .then((res) => {
+  //       if (!res.ok) throw new Error("Failed to fetch Data");
+  //       return res.json();
+  //     })
+  //     .then((data) => {
   //       console.log(data);
   //       setCoins(data);
-  //     } catch (err) {
-  //       setError(err.message);
-  //     } finally {
   //       setLoading(false);
-  //     }
-  //   };
-  //   fetchCoins();
+  //     })
+  //     .catch((err) => {
+  //       setError(err.message);
+  //       setLoading(false);
+  //     });
   // }, []);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+        );
+        if (!res.ok) throw new Error("Failed to fetch data!");
+        const data = await res.json();
+        console.log(data);
+        setCoins(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCoins();
+    // fetch(API_URL)
+    //   .then((res) => {
+    //     if (!res.ok) throw new Error("Failed to fetch Data");
+    //     return res.json();
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //     setCoins(data);
+    //     setLoading(false);
+    //   })
+    //   .catch((err) => {
+    //     setError(err.message);
+    //     setLoading(false);
+    //   });
+  }, []);
+
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <div className="'error"> {error}</div>}
+
+      {!loading && !error && (
+        <main className="grid">
+          {coins.map((coin) => (
+            <CoinCard key={coin.id} coin={coin}></CoinCard>
+          ))}
+        </main>
+      )}
     </div>
   );
 };
